a6612e77f6bb5c97b77b49ecf62b7b12e5046276
Damien de Lartigue <damien@driving.vision>
2021-05-19 18:21:07 +0100

    install react-router and create topbar component

---
+++ frontend/src/Topbar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import { TopBar, TopBarTitle } from 'react-foundation';
+
+class Topbar extends Component {
+	render() {
+			return <TopBar>
+			        <TopBarTitle>Coffee Reviews</TopBarTitle>
+			      </TopBar>
+		    }
+}
