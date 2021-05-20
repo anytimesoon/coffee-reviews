f0f3c8a08e3c9a510c4ff73dca65d8c35770e420
Damien de Lartigue <damien@driving.vision>
2021-05-20 13:58:50 +0100

    create Roasters Component

---
+++ frontend/src/Roasters.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {  Grid,
+          Cell } from 'react-foundation';
+
+class Roasters extends Component {
+	render() {
+		return  <div>
+	  	        <Grid className="display grid-padding-x grid-padding-y">
+								<h1>Roasters</h1>
+							</Grid>
+						</div>
+	}
+}
+
+export default Roasters
