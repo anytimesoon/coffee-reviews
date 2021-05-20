0b91702ed8e8292fd21d1ae39165ade3fbbceb91
Damien de Lartigue <damien@driving.vision>
2021-05-20 13:58:33 +0100

    create Coffees component

---
+++ frontend/src/Coffees.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {  Grid,
+          Cell } from 'react-foundation';
+
+class Coffees extends Component {
+	render() {
+		return  <div>
+	  	        <Grid className="display grid-padding-x grid-padding-y">
+								<h1>Coffees</h1>
+							</Grid>
+						</div>
+	}
+}
+
+export default Coffees
