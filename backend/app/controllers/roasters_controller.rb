7dffb2e128be729edd501002e2e35ad6736cca5d
Damien de Lartigue <damien@driving.vision>
2021-05-21 20:08:06 +0100

    amend show roaster route to include coffees is serialized hash

--- backend/app/controllers/roasters_controller.rb
+++ backend/app/controllers/roasters_controller.rb
@@ -1,13 +1,14 @@
 class RoastersController < ApplicationController
 	def index
 		roasters = Roaster.all
-		# binding.pry
+		
 		render json: RoasterSerializer.new(roasters).serializable_hash
 	end
 
 	def show
 		roaster = Roaster.find(params[:id])
-		# binding.pry
-		render json: RoasterSerializer.new(roaster).serializable_hash
+		options = {include: [:coffees]}
+		
+		render json: RoasterSerializer.new(roaster, options).serializable_hash
 	end
 end
