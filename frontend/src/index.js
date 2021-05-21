a6ab981d080178dcc110f5be0232900bd2f7ac5b
Damien de Lartigue <damien@driving.vision>
2021-05-21 16:30:32 +0100

    add redux dev tools to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 import 'foundation-sites/dist/css/foundation.min.css';
 import coffeesReducer from './reducers/coffeesReducer'
 
-const store = createStore(coffeesReducer, applyMiddleware(thunk))
+const store = createStore(coffeesReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 
 ReactDOM.render(
